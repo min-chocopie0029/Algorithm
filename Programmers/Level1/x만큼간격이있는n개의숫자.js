47af3833940158af6856880ae5c75218dd8b0bd7
min-chocopie <minchocopie29@gmail.com>
2021-11-12 20:27:46 +0900

    20211112 직사각형 별찍기

--- Programmers/Level1/x만큼간격이있는n개의숫자.js
+++ Programmers/Level1/x만큼간격이있는n개의숫자.js
@@ -1,7 +1,5 @@
 function solution(x, n) {
   let result = [x];
-  for (let i = 1; i < n; i++) {
-    result.push(result[i-1]+x);
-  }
+  for (let i = 1; i < n; i++) result.push(result[i-1]+x);
   return result;
 }
