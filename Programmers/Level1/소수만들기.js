4905c0159d22bdc41b5c98de69e7f4aa17f462c1
min-chocopie <minchocopie29@gmail.com>
2021-11-30 20:59:34 +0900

    20211130 소수 만들기

---
+++ Programmers/Level1/소수만들기.js
@@ -0,0 +1,21 @@
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) return false;
+  }
+  return n > 1;
+}
+
+function solution(nums) {
+  let result = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+      for (let k = j+1; k < nums.length; k++) {
+        let sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) result++;
+      }
+    }
+  }
+  
+  return result;
+}
